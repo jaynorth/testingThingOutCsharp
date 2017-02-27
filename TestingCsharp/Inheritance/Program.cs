7f6a0e1d1aef7fcfd521a5e8bb11a04af56fa53d
jayweb <jayweb@JAYWEB-PC>
2017-02-27 15:37:20 +0100

    feb27 done

---
+++ TestingCsharp/Inheritance/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Personne p = new Personne();
+            p.Conduire_override();
+
+            Pilote pilote = new Pilote();
+            pilote.Conduire_override();
+            pilote.Conduire_New();
+
+            Personne personnePilote = new Pilote();
+            personnePilote.Conduire_override();
+            personnePilote.Conduire_New();
+
+            //transtypage
+            Personne George = new Pilote();
+            George.Conduire_New();
+            Pilote Roger = (Pilote)George;
+
+            Roger.Conduire_New();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Personne
+    {
+
+        public virtual void Conduire_override()
+        {
+            Console.WriteLine("personne Conduire override");
+        }
+
+        public void Conduire_New()
+        {
+            Console.WriteLine("personne Conduire new");
+        }
+
+
+    }
+
+    public class Pilote : Personne
+    {
+        public override void Conduire_override()
+        {
+            Console.WriteLine("pilote conduire override");
+        }
+
+        public new void Conduire_New()
+        {
+            Console.WriteLine("pilote conduire new");
+        }
+    }
+}
---
+++ TestingCsharp/transtypage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transtypage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int shortnumber = 54;
+
+            long longnumber=69;
+
+            longnumber = shortnumber;
+            Console.WriteLine(longnumber);
+
+            longnumber = 34;
+            shortnumber = (int)longnumber;//implicit not possible
+            Console.WriteLine(shortnumber);
+            Console.ReadKey();
+        }
+    }
+}
